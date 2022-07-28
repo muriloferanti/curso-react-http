852ca8a8d10c430a787edfafda92e9bc15dace29
Murilo <murilo@astrusweb.com>
2022-07-28 13:01:47 -0300

    100% React with http

--- src/components/Usuarios/Usuarios.js
+++ src/components/Usuarios/Usuarios.js
@@ -8,10 +8,7 @@ class Usuarios extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      usuarios: [
-        { id: 1, nome: 'João', sobrenome: 'Silva', email: 'joao@mail.com' },
-        { id: 2, nome: 'Maria', sobrenome: 'Santos', email: 'maria@mail.com' }
-      ]
+      usuarios: []
     }
 
     this.adicionarUsuario = this.adicionarUsuario.bind(this)
@@ -24,10 +21,34 @@ class Usuarios extends Component {
 
   removerUsuario(usuario) {
     if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
-      let usuarios = this.state.usuarios
-      usuarios = usuarios.filter(x => x.id !== usuario.id)
-      this.setState({ usuarios: usuarios })
+      fetch(`https://reqres.in/api/users/${usuario.id}`, {
+        method: 'DELETE'
+      })
+        .then(resposta => { 
+          if(resposta.ok){
+            let usuarios = this.state.usuarios
+            usuarios = usuarios.filter(x => x.id !== usuario.id)
+            this.setState({ usuarios: usuarios })
+          }
+        })
     }
+  }
+
+  componentDidMount() {
+
+    fetch('https://reqres.in/api/users')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        const usuarios = dados.data.map(usuario => ({
+          id: usuario.id,
+          nome: usuario.first_name,
+          sobrenome: usuario.last_name,
+          email: usuario.email
+        }))
+
+        this.setState({ usuarios })
+      })
+
   }
 
   render() {
